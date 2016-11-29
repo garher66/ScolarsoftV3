4881bdf2fedfab3ffe29ea9fbd13bc94f8fb0dd5
francisco antonio garcia hernandez <francisco antonio garcia hernandez>
2016-11-28 19:12:53 -0600

    CONSULTAS MODIFICADAS

--- login.php
+++ login.php
@@ -98,12 +98,12 @@ where u.ID_USUARIO='".$ID_USUARIO."'";
 	print json_encode($datos);
 	//echo '<script>window.open("login.php?opcion=4","_blank")</script>';
 }
+//CONSULTAS MODIFICADAS
 if ($_GET['opcion']==4){
 
-	$sql="select f.id_folio,concat('pago de colegiatura del mes:',m.nombre) as historial,f.fecha,f.total,cn.precio,f.subtotal,f.descuento,
-f.recargo,'1' as cantidad, e.LOGO,e.COLEGIO,e.COLEGIO_DIRECCION,e.CORREO,e.COLEGIO_CONTACTO,concat(per.NOMBRE,' ',per.PATERNO,' ',per.MATERNO) as NOMBRE,
-gr.NOMBRE_GRADO as grado, g.NOMBRE_GRUPO as grupo,a.MATRICULA,n.NOMBRE_NIVEL as escolaridad
-from folio as f 
+	$sql="select '1' as cantidad, concat('pago de colegiatura del mes:',m.nombre) as descripcion,cn.precio,
+f.total,f.subtotal,f.recargo,f.descuento,f.pagado
+from folio as f
 inner join colegiatura as c on f.id_folio=c.id_folio
 inner join meses m on c.id_mes=m.id_mes
 inner join costo_nivel as cn on c.id_costo_nivel=cn.id_costo_nivel 
@@ -117,11 +117,9 @@ inner join grupos as g on g.ID_GRUPO=dg.ID_GRUPO
 inner join grado as gr on gr.ID_GRADO=dg.ID_GRADO
 inner join nivel_educativo as n on n.ID_NIVEL=dg.ID_NIVEL
 inner join ciclo_escolar as ci on ci.ID_CICLO=dg.ID_CICLO
-where u.ID_USUARIO=$ID_USUARIO and f.id_empresa=$ID_EMPRESA and f.id_ciclo=$ID_CICLO and f.id_folio=$ID_FOLIO
+where f.id_folio=$ID_FOLIO
 union 
-select f.id_folio, df.descripcion,f.fecha,f.total,df.precio,f.subtotal,f.descuento,f.recargo,df.cantidad,e.LOGO,
-e.COLEGIO,e.COLEGIO_DIRECCION,e.CORREO,e.COLEGIO_CONTACTO,concat(per.NOMBRE,' ',per.PATERNO,' ',per.MATERNO) as NOMBRE,
-gr.NOMBRE_GRADO as grado, g.NOMBRE_GRUPO as grupo,a.MATRICULA,n.NOMBRE_NIVEL as escolaridad
+select df.cantidad as cantidad,df.descripcion,df.precio,f.total,f.subtotal,f.recargo,f.descuento,f.pagado
 from  folio as f
 inner join detalle_folio as df  on f.id_folio=df.id_folio
 inner join productos as p on p.id_producto=df.id_producto
@@ -135,16 +133,14 @@ inner join grupos as g on g.ID_GRUPO=dg.ID_GRUPO
 inner join grado as gr on gr.ID_GRADO=dg.ID_GRADO
 inner join nivel_educativo as n on n.ID_NIVEL=dg.ID_NIVEL
 inner join ciclo_escolar as ci on ci.ID_CICLO=dg.ID_CICLO
-where u.ID_USUARIO=$ID_USUARIO and f.id_empresa=$ID_EMPRESA and f.id_ciclo=$ID_CICLO and f.id_folio=$ID_FOLIO";
-
+where f.id_folio=$ID_FOLIO";
+//http://localhost/android/login.php?ID_FOLIO=76&opcion=4
 	$rs=mysqli_query($con,$sql);
 	   $i=0;
 	   while ($row=mysqli_fetch_array($rs)) {	
 
-	   	$datos[$i]=array("id_folio"=>$row[0],"historial"=>$row[1],"fecha"=>$row[2],"total"=>$row[3],"precio"=>$row[4],
-	   		"subtotal"=>$row[5],"descuento"=>$row[6],"recargo"=>$row[7],"cantidad"=>$row[8],"LOGO"=>$row[9],"COLEGIO"=>$row[10],
-	   		"COLEGIO_DIRECCION"=>$row[11],"CORREO"=>$row[12],"COLEGIO_CONTACTO"=>$row[13],"NOMBRE"=>$row[14],"grado"=>$row[15],
-	   		"grupo"=>$row[16],"MATRICULA"=>$row[17],"escolaridad"=>$row[18]);
+	   	$datos[$i]=array("cantidad"=>$row[0],"descripcion"=>$row[1],"precio"=>$row[2],"total"=>$row[3],"subtotal"=>$row[4],
+	   		"recargo"=>$row[5],"descuento"=>$row[6],"pagado"=>$row[7]);
 	   		$i=$i+1; 
 	   	}
 	   		$rs = mysqli_query($con,$sql) or die (mysql_error());
@@ -189,6 +185,7 @@ inner join grado as gr on gr.ID_GRADO=dg.ID_GRADO
 inner join nivel_educativo as n on n.ID_NIVEL=dg.ID_NIVEL
 inner join ciclo_escolar as ci on ci.ID_CICLO=dg.ID_CICLO
 where u.ID_USUARIO=$ID_USUARIO ";
+//http://localhost/android/login.php?ID_USUARIO=3&opcion=5
 
 $rs=mysqli_query($con,$sql);
 	   $i=0;
@@ -222,7 +219,7 @@ inner join grupos as g on g.ID_GRUPO=dg.ID_GRUPO
 inner join grado as gr on gr.ID_GRADO=dg.ID_GRADO
 inner join nivel_educativo as n on n.ID_NIVEL=dg.ID_NIVEL
 inner join ciclo_escolar as ci on ci.ID_CICLO=dg.ID_CICLO
-where  f.id_folio=$ID_FOLIO
+where f.id_folio=$ID_FOLIO
 union 
 select df.cantidad as cantidad,df.descripcion,df.precio
 from  folio as f
@@ -239,6 +236,7 @@ inner join grado as gr on gr.ID_GRADO=dg.ID_GRADO
 inner join nivel_educativo as n on n.ID_NIVEL=dg.ID_NIVEL
 inner join ciclo_escolar as ci on ci.ID_CICLO=dg.ID_CICLO
 where f.id_folio=$ID_FOLIO";
+//http://localhost/android/login.php?ID_FOLIO=76&opcion=6
 $rs=mysqli_query($con,$sql);
 	   $i=0;
 	   while ($row=mysqli_fetch_array($rs)) {	
@@ -259,53 +257,7 @@ if (mysqli_num_rows($rs)>0)
 
 /*
 
-if ($_GET['opcion']==4){
-
-	$sql= "select id_folio,id_usuario,group_concat(' ',des) as historial,fecha,total,subtotal,descuento,recargo,
-case when estatus=0 then 'Sin pagar' when estatus=1 then 'Pagado' End as status 
-from
-(
-(select f.id_folio as id_folio,pf.id_usuario as id_usuario,
-concat('pago de mes:',m.nombre) as des,date(f.fecha) as fecha,f.total as total,
-f.subtotal as subtotal,f.descuento as descuento,f.recargo as recargo,f.pagado as estatus 
-from 
-folio as f  
-inner join colegiatura as c  on f.id_folio=c.id_folio
-inner join meses as m on c.id_mes=m.id_mes
-inner join pago_folio as pf on pf.id_folio=f.id_folio
-where pf.id_usuario=$ID_USUARIO and f.id_ciclo= $ID_CICLO 	
-)union 
-(select f.id_folio as id_folio,pf.id_usuario as id_usuario,pro.descripcion as des, date(f.fecha) as fecha,f.total as total,
-f.subtotal as subtotal,f.descuento as descuento,f.recargo as recargo,f.pagado as estatus
-from folio as  f 
-inner join detalle_folio as df on f.id_folio=df.id_folio
-inner join productos as pro on  pro.id_producto=df.id_producto
-inner join pago_folio as pf on pf.id_folio=f.id_folio
-where pf.id_usuario=$ID_USUARIO and f.id_ciclo=$ID_CICLO 
-)
-) as tablauni
-group by id_folio ";
-	$rs=mysqli_query($con,$sql);
-	   $i=0;
-	   while ($row=mysqli_fetch_array($rs)) {	
-		
-		//$datos[$i]=array("ID_ASIGNATURA"=>$row[0],"NOMBRE_ASIGNATURA"o=>$rw[1]);
-		
-
-		$datos[$i]=array("id_folio"=>$row[0],"id_usuario"=>$row[1],"historial"=>$row[2],"fecha"=>$row[3],"total"=>$row[4],"subtotal"=>$row[5],"descuento"=>$row[6],"recargo"=>$row[7],"status"=>$row[8]);
-			$i=$i+1; 
-	}
-
-$rs = mysqli_query($con,$sql) or die (mysql_error());
-if (mysqli_num_rows($rs)>0)
-{
-	print json_encode($datos);
-}else {
-	print ('No se encontraron datos');
-}
-	
-}
-if ($_GET['opcion']==5){
+if ($_GET['opcion']==){
 
 $sql="select f.id_folio,concat('pago de colegiatura del mes:',m.nombre),f.fecha,f.total,cn.precio,f.subtotal,f.descuento,
 f.recargo,'1' as cantidad, e.LOGO,e.COLEGIO,e.COLEGIO_DIRECCION,e.CORREO,e.COLEGIO_CONTACTO 
